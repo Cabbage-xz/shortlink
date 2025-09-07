15cb1f1eb622f9c1d0212b9ad6a3330e8519f881
xzcabbage <xzcabbage@gmail.com>
2025-09-07 14:29:24 +0800

    feat：定义异常码
    desc：定义异常码

---
+++ admin/src/main/java/org/cabbage/shortlink/admin/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,21 @@
+package org.cabbage.shortlink.admin.common.convention.errorcode;
+
+/**
+ * @author xzcabbage
+ * @since 2025/9/7
+ */
+/**
+ * 平台错误码
+ */
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
--- admin/src/main/java/org/cabbage/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/cabbage/shortlink/admin/controller/UserController.java
@@ -2,6 +2,7 @@ package org.cabbage.shortlink.admin.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.cabbage.shortlink.admin.common.convention.result.Result;
+import org.cabbage.shortlink.admin.common.enums.UserErrorCodeEnum;
 import org.cabbage.shortlink.admin.dto.resp.UserRespDTO;
 import org.cabbage.shortlink.admin.service.interfaces.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,8 +25,8 @@ public class UserController {
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
         UserRespDTO user = userService.getUserByUsername(username);
         if (user == null) {
-            return new Result<UserRespDTO>().setCode("-1").setMessage("User not found");
+            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
         }
-        return new Result<UserRespDTO>().setCode("0").setData(user);
+        return new Result<UserRespDTO>().setCode(Result.SUCCESS_CODE).setData(user);
     }
 }
