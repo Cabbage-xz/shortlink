c21571a3fe1fa46434291b3d50d4116b755e0fcd
xzcabbage <xzcabbage@gmail.com>
2025-09-21 17:08:54 +0800

    feat：应用上下文添加用户信息 desc：应用上下文添加用户信息

---
+++ admin/src/main/java/org/cabbage/shortlink/admin/config/UserConfiguration.java
@@ -0,0 +1,24 @@
+package org.cabbage.shortlink.admin.config;
+
+import org.cabbage.shortlink.admin.common.biz.user.UserTransmitFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * @author xzcabbage
+ * @since 2025/9/21
+ */
+@Configuration
+public class UserConfiguration {
+
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter(StringRedisTemplate stringRedisTemplate) {
+        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserTransmitFilter(stringRedisTemplate));
+        registration.addUrlPatterns("/*");
+        registration.setOrder(0);
+        return registration;
+    }
+}
