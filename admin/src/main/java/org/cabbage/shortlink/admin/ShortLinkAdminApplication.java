d9f1b9773f4e4ecb67138f6ba9341f332d2417be
xzcabbage <xzcabbage@gmail.com>
2025-09-25 23:12:01 +0800

    feat：添加全局组件扫描 desc：添加全局组件扫描

--- admin/src/main/java/org/cabbage/shortlink/admin/ShortLinkAdminApplication.java
+++ admin/src/main/java/org/cabbage/shortlink/admin/ShortLinkAdminApplication.java
@@ -3,9 +3,11 @@ package org.cabbage.shortlink.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("org.cabbage.shortlink.admin.dao.mapper")
+@ComponentScan("org.cabbage.shortlink")
 public class ShortLinkAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkAdminApplication.class, args);
--- project/src/main/java/org/cabbage/shortlink/project/ShortLinkApplication.java
+++ project/src/main/java/org/cabbage/shortlink/project/ShortLinkApplication.java
@@ -3,9 +3,11 @@ package org.cabbage.shortlink.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("org.cabbage.shortlink.project.dao.mapper")
+@ComponentScan("org.cabbage.shortlink")
 public class ShortLinkApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkApplication.class, args);
