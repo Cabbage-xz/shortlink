c9da2b072f3bbf1d8ab37354742f41f866be574a
xzcabbage <xzcabbage@gmail.com>
2025-10-01 18:47:48 +0800

    feat：短链接分组表分表 desc：短链接分组表分表

--- project/src/test/java/org/cabbage/shortlink/project/test/LinkTableShardingTest.java
+++ project/src/test/java/org/cabbage/shortlink/project/test/LinkTableShardingTest.java
@@ -29,9 +29,27 @@ public class LinkTableShardingTest {
             "        unique (full_short_url)\n" +
             ");";
 
+    public static final String SQL_GROUP = "create table t_group_%d\n" +
+            "            (\n" +
+            "                    id          bigint auto_increment comment 'ID'\n" +
+            "                            primary key,\n" +
+            "                    gid         varchar(32)  null comment '分组标识',\n" +
+            "    name        varchar(64)  null comment '分组名称',\n" +
+            "    username    varchar(256) null comment '创建分组用户名',\n" +
+            "    sort_order  int          null comment '分组排序',\n" +
+            "    create_time datetime     null comment '创建时间',\n" +
+            "    update_time datetime     null comment '修改时间',\n" +
+            "    del_flag    tinyint(1)   null comment '删除标识 0：未删除 1：已删除',\n" +
+            "    constraint idx_unique_username_gid\n" +
+            "    unique (gid, username)\n" +
+            ");";
+
+
+
+
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
-            System.out.printf((SQL) + "%n", i);
+            System.out.printf((SQL_GROUP) + "%n", i);
         }
     }
 }
