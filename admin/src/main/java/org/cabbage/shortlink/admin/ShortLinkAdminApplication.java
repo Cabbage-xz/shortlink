8d200a11ee2098316c029812f3bb88429575a3b4
xzcabbage <xzcabbage@gmail.com>
2025-11-03 22:22:02 +0800

    feat：引入nacos desc：引入nacos

--- admin/src/main/java/org/cabbage/shortlink/admin/ShortLinkAdminApplication.java
+++ admin/src/main/java/org/cabbage/shortlink/admin/ShortLinkAdminApplication.java
@@ -3,11 +3,15 @@ package org.cabbage.shortlink.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("org.cabbage.shortlink.admin.dao.mapper")
 @ComponentScan("org.cabbage.shortlink")
+@EnableDiscoveryClient
+@EnableFeignClients("org.cabbage.shortlink.admin.remote")
 public class ShortLinkAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkAdminApplication.class, args);
--- project/src/main/java/org/cabbage/shortlink/project/ShortLinkApplication.java
+++ project/src/main/java/org/cabbage/shortlink/project/ShortLinkApplication.java
@@ -3,11 +3,13 @@ package org.cabbage.shortlink.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("org.cabbage.shortlink.project.dao.mapper")
 @ComponentScan("org.cabbage.shortlink")
+@EnableDiscoveryClient
 public class ShortLinkApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkApplication.class, args);
